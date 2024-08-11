19fa3cae390668dc8cf07e73ef91093cfa78c7d6
luandersonaOliveira <luandersona.oliveira@unifacol.edu.br>
2024-08-11 20:57:47 -0300

    Atividades

--- Atividades/Atividade06/src/atividade/Main.java
+++ Atividades/Atividade06/src/atividade/Main.java
@@ -1,10 +1,31 @@
 package atividade;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
-  public static void main(String[] args) {
-    
-  }
-  
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            numeros.add(random.nextInt(100));
+        }
+
+        System.out.println("Lista: " + numeros);
+
+        int valorParaBuscar = 10;
+        int indice = buscarValor(numeros, valorParaBuscar);
+        System.out.println("Índice do valor " + valorParaBuscar + ": " + indice);
+    }
+
+    public static int buscarValor(ArrayList<Integer> numeros, int valor) {
+        for (int i = 0; i < numeros.size(); i++) {
+            if (numeros.get(i) == valor) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
