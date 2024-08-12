074467ba51df25cf402bb3745d47aa8bdde65312
luandersonaOliveira <luandersona.oliveira@unifacol.edu.br>
2024-08-12 15:56:10 -0300

    Atividades

--- Atividades/Atividade08/src/atividade/Main.java
+++ Atividades/Atividade08/src/atividade/Main.java
@@ -1,7 +1,27 @@
 package atividade;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] array = { 2, 4, 6 };
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int num : array) {
+            arrayList.add(num);
+        }
+        arrayList.add(1);
+        arrayList.add(3);
+        arrayList.add(5);
+
+        System.out.println(arrayList);
+
+        Integer[] newArray = new Integer[arrayList.size()];
+        newArray = arrayList.toArray(newArray);
+
+        for (int nums : newArray) {
+            System.out.print(nums + " ");
+        }
     }
 }
