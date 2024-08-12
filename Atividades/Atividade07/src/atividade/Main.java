e53a9b80740f0fec905e5423bd25351034fb3885
luandersonaOliveira <luandersona.oliveira@unifacol.edu.br>
2024-08-11 22:01:14 -0300

    Atividades

--- Atividades/Atividade07/src/atividade/Main.java
+++ Atividades/Atividade07/src/atividade/Main.java
@@ -1,7 +1,18 @@
 package atividade;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<String> cidades = new ArrayList<String>();
+        cidades.add("Buíque");
+        cidades.add("Bom Conselho");
+        cidades.add("Brejao");
+        cidades.add("Venturosa");
+        cidades.add("Canhotinho");
+        System.out.println("Lista de cidades antes da ordenação: \n" + cidades);
+        Collections.sort(cidades);
+        System.out.println("Lista de cidades após a ordenação: \n" + cidades);
     }
 }
